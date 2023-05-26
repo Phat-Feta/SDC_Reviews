a8c783c0d183e3c5f8f77416f94a68488d523e98
Naru Sadakuni <narusadakuni1@gmail.com>
2023-05-26 15:49:51 -0700

    hashed out getReviews and getMeta

--- server/controllers.js
+++ server/controllers.js
@@ -3,31 +3,66 @@ const models = require('./models.js');
 const getReviews = (req, res) => {
   console.log(req.query);
   const query =
-  `SELECT * FROM reviews WHERE product=${Number(req.query.product_id)} LIMIT ${Number(req.query.counts)};`
+  `SELECT * FROM reviews WHERE product=${Number(req.query.product_id)} LIMIT ${Number(req.query.counts) || 5};`
   models.get(query)
     .then(({rows}) => {res.status(201).send(rows)})
     .catch(err => res.status(500).send(err))
 }
 
 const getMeta = (req, res) => {
-  let body = {};
-  let ratings = {};
-  let recommened = {};
-  let characteristics = {};
+  let body = {
+    ratings: {},
+    recommended: {},
+    characteristics: {}
+  };
   const queryMeta =
-  `SELECT * FROM meta WHERE product_id=${Number(req.query.product_id)} UNION ALL
-  SELECT * FROM characteristics WHERE id=${Number(req.query.product_id)} UNION ALL
-  SELECT * FROM recommended WHERE product_id=${Number(req.query.product_id)};`
+  `SELECT * FROM meta WHERE product_id=${Number(req.query.product_id)};`
   models.get(queryMeta)
     .then(({rows}) => {
-      console.log(rows)
       body.product_id = rows[0].product_id;
-      ratings['1'] = rows[0].one;
-      ratings['2'] = rows[0].two;
-      ratings['3'] = rows[0].three;
-      ratings['4'] = rows[0].four;
-      ratings['5'] = rows[0].five;
-      body.ratings = ratings;
+      body.ratings = {
+        '1': rows[0].one,
+        '2': rows[0].two,
+        '3': rows[0].three,
+        '4': rows[0].four,
+        '5': rows[0].five
+      }
+    })
+    .then(() => {
+      const queryCharacteristics =
+      `SELECT * FROM characteristics WHERE product_id=${Number(req.query.product_id)};`
+      return models.get(queryCharacteristics)
+    })
+    .then(({rows}) => {
+      let chars = {
+        Quality: {id: 0, value: []},
+        Comfort: {id: 0, value: []},
+        Fit: {id: 0, value: []},
+        Length: {id: 0, value: []},
+        Width: {id: 0, value: []},
+        Size: {id: 0, value: []}
+      };
+      for (let i = 0; i < rows.length; i++) {
+        chars[rows[i].name].value.push(rows[i].value)
+        chars[rows[i].name].id = rows[i].characteristic_id
+      }
+      for (var key in chars) {
+        if (chars[key].id) {
+          body.characteristics[key] = {
+            id: chars[key].id,
+            value: chars[key].value.reduce((a,b) => a+b)/chars[key].value.length
+          }
+        }
+      }
+    })
+    .then(() => {
+      const queryRecommended =
+      `SELECT * FROM recommended WHERE product_id=${Number(req.query.product_id)};`
+      return models.get(queryRecommended);
+    })
+    .then(({rows}) => {
+      body.recommended.true = rows[0].true;
+      body.recommended.false = rows[0].false;
     })
     .then(() => res.status(201).send(body))
     .catch(err => res.status(500).send(err))
