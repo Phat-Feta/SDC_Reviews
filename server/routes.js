3af06091b50a645ccfd5f55e20eb0b2c0bbba099
Naru Sadakuni <narusadakuni1@gmail.com>
2023-05-27 16:00:18 -0700

    added logic for empty photos and also changing variable types before sending back response

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const controllers = require('./controllers.js');
 
 router.get('/reviews', controllers.getReviews);
-router.get('/meta', controllers.getMeta);
+router.get('/reviews/meta', controllers.getMeta);
 router.post('/reviews', controllers.postReview)
 router.put('/reviews/:review_id/helpful', controllers.putHelpful)
 router.put('/reviews/:review_id/report', controllers.putReport)
